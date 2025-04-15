5c7fb306d17cd6c44a4b5ea91928f05b3d220f3d
Milan Kracht <milan@domini.nl>
2025-04-15 11:48:00 +0200

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts"></script>
+
+<template>
+  <header>
+    <p>This is a header</p>
+  </header>
+
+  <RouterView />
+</template>
---
+++ src/views/ShowView.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts"></script>
+
+<template>
+  <main>
+    <h1>Show details</h1>
+  </main>
+</template>
